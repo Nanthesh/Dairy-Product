bda255a6d21282edc5452c6f1244ee2d4fbec7aa
Nanthesh <nanthesh8kannan@gmail.com>
2023-10-26 11:24:50 -0400

    object added

---
+++ Objects.js
@@ -0,0 +1,80 @@
+const categories = {
+    dairy: {
+      name: "Dairy Products",
+      description: "A variety of dairy items for your daily needs.",
+    },
+    eggs: {
+      name: "Eggs",
+      description: "Fresh and farm-sourced eggs for your kitchen.",
+    },
+    coconut: {
+      name: "Coconut Products",
+      description: "Products related to fresh coconuts and coconut oil.",
+    },
+  };
+  const dairyProducts = {
+    ghee: {
+      name: "Ghee",
+      description: "Pure clarified butter. Ideal for cooking and adding flavor to your dishes.",
+      price: 10.99,
+      weight: "500ml",
+      category: categories.dairy,
+    },
+    milk: {
+      name: "Milk",
+      description: "Fresh cow's milk. A nutritious choice for daily consumption.",
+      price: 2.99,
+      volume: "1 liter",
+      category: categories.dairy,
+    },
+    egg: {
+      name: "Egg",
+      description: "Farm-fresh eggs. A versatile ingredient for a wide range of dishes.",
+      price: 0.99,
+      quantity: "1 dozen",
+      category: categories.eggs,
+    },
+    buttermilk: {
+      name: "Buttermilk",
+      description: "Refreshing buttermilk. Perfect for quenching your thirst on a hot day.",
+      price: 1.49,
+      volume: "500ml",
+      category: categories.dairy,
+    },
+    bread: {
+      name: "Bread",
+      description: "Whole wheat bread. A staple for making sandwiches and toast.",
+      price: 2.49,
+      quantity: "1 loaf",
+      category: categories.dairy,
+    },
+    curd: {
+      name: "Curd",
+      description: "Thick and creamy curd. Enjoy it plain or as a side with your meals.",
+      price: 3.49,
+      volume: "500g",
+      category: categories.dairy,
+    },
+    paneer: {
+      name: "Paneer",
+      description: "Fresh paneer cheese. Use it in various Indian and international dishes.",
+      price: 4.99,
+      weight: "250g",
+      category: categories.dairy,
+    },
+    coconut: {
+      name: "Coconut",
+      description: "Fresh coconut. Extract coconut water and use the flesh for cooking.",
+      price: 1.99,
+      quantity: "1 coconut",
+      category: categories.coconut,
+    },
+    virginCoconutOil: {
+      name: "Virgin Coconut Oil",
+      description: "Pure virgin coconut oil. Great for cooking and skincare.",
+      price: 5.99,
+      volume: "250ml",
+      category: categories.coconut,
+    },
+  };
+  
